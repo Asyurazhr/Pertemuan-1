5781d3d5d579c939b9b44cf39b94f24a951c7e99
Asyurazhr <Asyuradjola2004@gmail.com>
2026-05-09 22:07:46 +0700

    Pertemuan 2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <style>
+        body{
+            font-family: Arial;
+            background:#f5f5f5;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            height:100vh;
+        }
+
+        .card{
+            background:white;
+            padding:40px;
+            border-radius:15px;
+            box-shadow:0 0 15px rgba(0,0,0,0.2);
+            width:400px;
+        }
+
+        h1{
+            text-align:center;
+            color:#e63946;
+        }
+
+        p{
+            font-size:18px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="card">
+    <h1>Biodata</h1>
+
+    <p><b>Nama:</b> Asyura Azzahra</p>
+    <p><b>NIM:</b> 20230140231</p>
+    <p><b>Program Studi:</b> Teknologi Informasi</p>
+    <p><b>Hobi:</b> Main Game</p>
+</div>
+
+</body>
+</html>
