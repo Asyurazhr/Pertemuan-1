387cca07b7c133d5ad5696417d30b30ffcd11b8f
Asyurazhr <Asyuradjola2004@gmail.com>
2026-05-09 21:24:46 +0700

    Pertemuan 1 Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1</title>
+
+    <style>
+        body{
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            height:100vh;
+        }
+
+        .card{
+            background:white;
+            padding:40px;
+            border-radius:15px;
+            box-shadow:0 0 15px rgba(0,0,0,0.2);
+            text-align:center;
+            width:350px;
+        }
+
+        h1{
+            color:#e63946;
+        }
+
+        h2{
+            color:#333;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="card">
+        <h1>Nama: Asyura Azzahra</h1>
+        <h2>NIM: 20230140231</h2>
+    </div>
+
+</body>
+</html>
